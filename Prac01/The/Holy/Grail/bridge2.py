a52b9bf6902c56a8d94fe2f18d139daaf79d8363
Sylvester-21828911 <21828911@student.curtin.edu.au>
2024-03-10 14:45:04 +0800

    Initial commit

---
+++ Prac01/The/Holy/Grail/bridge2.py
@@ -0,0 +1,15 @@
+#
+# bridge.py - a scene from The Holy Grail
+#
+
+print("\nWelcome to the Bridge of Death\n")
+
+name = input("What is your name?\n")
+
+quest = input("What is your quest?\n")
+
+colour = input("What is your favourite colour?\n")
+
+print("Hello,", name, "good luck with your", quest, "quest!")
+print("Perhaps wearing", colour, "socks would help :)\n")
+
