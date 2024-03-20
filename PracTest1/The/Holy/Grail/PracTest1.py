7a47695ce2f475ced77b5d851e1d169e8bac0b1a
Sylvester-21828911 <21828911@student.curtin.edu.au>
2024-03-20 15:01:53 +0800

    Somewhat finished PracTest1 (need to check the layout again, but mostly functional)

--- PracTest1/The/Holy/Grail/PracTest1.py
+++ PracTest1/The/Holy/Grail/PracTest1.py
@@ -5,9 +5,11 @@
 # Student ID: 21828911
 ##
 
+from datetime import datetime
+
 myname = "Sylvester"
 myyear = 1996
-currentyear = 2024
+currentyear = int(datetime.today().strftime('%Y'))
 
 print("Hello, my name is ", myname)
 uname = input("What is your name?")
@@ -20,16 +22,26 @@ while True:
             break
     except ValueError:
         print("You haven't told me anything!")
+
 age = currentyear - uyear
 print("Birthday greetings,", uname,"the aged!")
 print("Here's a cake with", age, "candles!")
-
 print(end=" ")
-for i in range(age//2):  
+if age % 2 == 1:          #add space for odd years
+    print(end=" ")
+for i in range(age//2):     #print first line of candles
     print("*", end=" ")
 print("")
-if age % 2 != 0:
+if age % 2 != 0:            #add space in the end for odd years
     print(end=" ")
-for i in range(age//2):
+for i in range(age//2):     #print second line of candles
     print("*",end="|")
+print("")
+for i in range(age//2):     #second line of candle sticks
+    print(end="| ")
+print("")
+for i in range(age):
+    print(end="#")
+
+
 print()
