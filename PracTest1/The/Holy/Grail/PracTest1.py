e84342f7074c489de1966d8c061cdaebd89aa87b
Sylvester-21828911 <21828911@student.curtin.edu.au>
2024-03-25 13:17:05 +0800

    Finished PracTest1

--- PracTest1/The/Holy/Grail/PracTest1.py
+++ PracTest1/The/Holy/Grail/PracTest1.py
@@ -5,43 +5,50 @@
 # Student ID: 21828911
 ##
 
-from datetime import datetime
-
 myname = "Sylvester"
 myyear = 1996
-currentyear = int(datetime.today().strftime('%Y'))
+maxyear = 2020
 
 print("Hello, my name is ", myname)
 uname = input("What is your name?")
 while True:
     try:
         uyear = int(input("What year were you born?"))
-        if 1950 > uyear or uyear > currentyear:
+        if 1930 > uyear or uyear > maxyear: #check if year is valid
             print("You can't be that old! Try again")
         else:
             break
-    except ValueError:
+    except ValueError:      #if user input nothing
         print("You haven't told me anything!")
 
-age = currentyear - uyear
+age = maxyear - uyear       
 print("Birthday greetings,", uname,"the aged!")
+if uyear > myyear :         #check if younger or older than me
+    print('Seems like you\'re younger than me!')
+else:
+    print('Seems like you\'re older than me!')
 print("Here's a cake with", age, "candles!")
+
 print(end=" ")
-if age % 2 == 1:          #add space for odd years
-    print(end=" ")
 for i in range(age//2):     #print first line of candles
     print("*", end=" ")
 print("")
-if age % 2 != 0:            #add space in the end for odd years
-    print(end=" ")
 for i in range(age//2):     #print second line of candles
     print("*",end="|")
 print("")
 for i in range(age//2):     #second line of candle sticks
     print(end="| ")
 print("")
-for i in range(age):
-    print(end="#")
 
+if age % 2 == 1:
+    age = age -1
 
+for g in range(5):          #cake base
+    if g % 2 != 0:
+        for i in range(age):
+            print(end="#")
+    else:
+        for i in range(age):
+            print(end='=')
+    print()
 print()
